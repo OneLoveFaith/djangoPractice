1d63359a128b0911116ae88374fa5e0929cb5177
OneLoveFaith <oneloveshinobu@gmail.com>
2023-10-11 12:55:05 +0600

    initial

---
+++ book/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+class Book(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    image = models.ImageField(upload_to='book_covers/')
+    cost = models.DecimalField(max_digits=10, decimal_places=2)
+    created_date = models.DateTimeField(auto_now_add=True)
+
+
+    class Meta:
+        app_label = 'book'
+
